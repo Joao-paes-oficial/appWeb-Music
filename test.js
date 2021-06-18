54cfa502b138dd880b374ba44241c470cfa8583c
João Victor Paes Barreto <joao.paes.oficial@gmail.com>
2021-06-17 22:55:22 -0300

    Tamanho da pasta
    Verificar quantos arquivos tem na pasta music.

---
+++ test.js
@@ -0,0 +1,28 @@
+const fs = require('fs').promises;
+
+async function listarArquivosDoDiretorio(diretorio, arquivos) {
+
+    if(!arquivos)
+        arquivos = [];
+
+    let listaDeArquivos = await fs.readdir(diretorio);
+    for(let k in listaDeArquivos) {
+        let stat = await fs.stat(diretorio + '/' + listaDeArquivos[k]);
+        if(stat.isDirectory())
+            await listarArquivosDoDiretorio(diretorio + '/' + listaDeArquivos[k], arquivos);
+        else
+            arquivos.push(diretorio + '/' + listaDeArquivos[k]);
+    }
+
+    return arquivos;
+
+}
+
+async function test() {
+    let arquivos = await listarArquivosDoDiretorio('./music'); // coloque o caminho do seu diretorio
+    console.log(arquivos.length);
+    return arquivos;
+
+}
+
+test();
